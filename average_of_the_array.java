9bdda5988062d537d68362654654e81ea4369e19
Pothulakalyan <96769120+Pothulakalyan@users.noreply.github.com>
2022-10-05 21:38:09 +0530

    import java.util.*; class file {     public static void main(String[] args)     {         Scanner k=new Scanner(System.in);         int n,sum=0;         n=k.nextInt();         int arr[]=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=k.nextInt();             sum=sum+arr[i];         }         double avg=(float)sum/n;         System.out.format("%.2f",avg);     } }

--- average_of_the_array.java
+++ average_of_the_array.java
@@ -4,19 +4,15 @@ class file
     public static void main(String[] args)
     {
         Scanner k=new Scanner(System.in);
-        int n,sum=0,count=0;
+        int n,sum=0;
         n=k.nextInt();
         int arr[]=new int[n];
         for(int i=0;i<n;i++)
         {
             arr[i]=k.nextInt();
-        }
-        for(int i=0;i<n;i++)
-        {
             sum=sum+arr[i];
-            count++;
         }
-        double avg=(float)sum/count;
+        double avg=(float)sum/n;
         System.out.format("%.2f",avg);
     }
 }
