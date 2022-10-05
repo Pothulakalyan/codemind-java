87e5c873b31e55e18361e67bc6feab7b93b218ee
Pothulakalyan <96769120+Pothulakalyan@users.noreply.github.com>
2022-10-05 17:34:07 +0530

    import java.util.*; class file {     public static void main(String[] args)     {         Scanner k=new Scanner(System.in);         int P,T,R,s;         P=k.nextInt();         T=k.nextInt();         R=k.nextInt();         s=(P*T*R)/100;         System.out.println(s);     }
    }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class file
+{
+    public static void main(String[] args)
+    {
+        Scanner k=new Scanner(System.in);
+        int P,T,R,s;
+        P=k.nextInt();
+        T=k.nextInt();
+        R=k.nextInt();
+        s=(P*T*R)/100;
+        System.out.println(s);
+    }
+    
+    
+}
