fdf1d16191fe698f9d34a7411f8b56f46e138bad
Pothulakalyan <96769120+Pothulakalyan@users.noreply.github.com>
2022-10-05 12:43:38 +0530

    import java.util.*; class file {     public static void main(String[] args)     {         Scanner k=new Scanner(System.in);         int n,sum=0;         n=k.nextInt();         for(int i=1;i<n;i++)         {             if(n%i==0)             {                 sum=sum+i;             }         }         if(sum==n)         {             System.out.println("True");         }         else         {             System.out.println("False");         }     } }

---
+++ Perfect_Number.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class file
+{
+    public static void main(String[] args)
+    {
+        Scanner k=new Scanner(System.in);
+        int n,sum=0;
+        n=k.nextInt();
+        for(int i=1;i<n;i++)
+        {
+            if(n%i==0)
+            {
+                sum=sum+i;
+            }
+        }
+        if(sum==n)
+        {
+            System.out.println("True");
+        }
+        else
+        {
+            System.out.println("False");
+        }
+    }
+}
