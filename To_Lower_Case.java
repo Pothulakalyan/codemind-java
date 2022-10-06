13e615c08c6cda55ff40d3fc08cdb0596e5f85ae
Pothulakalyan <96769120+Pothulakalyan@users.noreply.github.com>
2022-10-07 00:07:07 +0530

    import java.util.*; class file {     public static void main(String[] args)     {         Scanner k=new Scanner(System.in);         String s1=k.nextLine();         String a=s1.toLowerCase();         System.out.println(a);     } }

---
+++ To_Lower_Case.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class file
+{
+    public static void main(String[] args)
+    {
+        Scanner k=new Scanner(System.in);
+        String s1=k.nextLine();
+        String a=s1.toLowerCase();
+        System.out.println(a);
+    }
+}
