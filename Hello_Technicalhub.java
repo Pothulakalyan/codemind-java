a9bc9d784cea7c6fd432ddd191835de4e9df669e
Pothulakalyan <96769120+Pothulakalyan@users.noreply.github.com>
2022-10-10 23:00:44 +0530

    import java.util.*; class file {     public static void main(String[] args)     {         Scanner k=new Scanner(System.in);         String s1=k.nextLine();         System.out.println("Hello Technicalhub");         System.out.println(s1+"");     } }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class file
+{
+    public static void main(String[] args)
+    {
+        Scanner k=new Scanner(System.in);
+        String s1=k.nextLine();
+        System.out.println("Hello Technicalhub");
+        System.out.println(s1+"");
+    }
+}
