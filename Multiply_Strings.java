5d9cbeb928f24fac553768a56cdcf46cca9990f5
Pothulakalyan <96769120+Pothulakalyan@users.noreply.github.com>
2022-12-20 12:18:20 +0530

    import java.util.*; class file {     public static void main(String args[])     {         Scanner k=new Scanner(System.in);         String s1=k.next();         String s2=k.next();         long a=Integer.parseInt(s1);         long b=Integer.parseInt(s2);         System.out.println(a*b);     } }

---
+++ Multiply_Strings.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class file
+{
+    public static void main(String args[])
+    {
+        Scanner k=new Scanner(System.in);
+        String s1=k.next();
+        String s2=k.next();
+        long a=Integer.parseInt(s1);
+        long b=Integer.parseInt(s2);
+        System.out.println(a*b);
+    }
+}
